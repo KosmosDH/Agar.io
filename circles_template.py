8ed1b9a5cd13c00c90f4fbea3c60cfc936bd952f
KosmosDH <koly1411@list.ru>
2021-07-09 19:12:38 +1000

    add: Pause, exit, player, music, colors, size

--- circles_template.py
+++ circles_template.py
@@ -12,6 +12,11 @@ class Circle(arcade.Sprite):
         super().__init__("circle.png", scale)
         sides = ["left", "top", "right", "bottom"]
         self.side = random.choice(sides)
+        self.color = (
+            random.randint(50, 200),
+            random.randint(50, 200),
+            random.randint(50, 200)
+        )
         if self.side == "left":
             self.right = 0
             self.center_y = random.randint(0, SCREEN_HEIGHT)
@@ -46,31 +51,83 @@ class Circle(arcade.Sprite):
             self.kill()
 
 
+class Player(Circle):
+    def __init__(self):
+        super().__init__(0.3)
+        self.center_x = SCREEN_WIDTH // 2
+        self.center_y = SCREEN_HEIGHT // 2
+        self.change_x = 0
+        self.change_y = 0
+        self.color = arcade.color.WHITE
+
+
 class OurGame(arcade.Window):
     def __init__(self, width, height, title):
-        super().__init__(width, height, title)
+        super().__init__(width, height, title, fullscreen= True)
+        global SCREEN_WIDTH, SCREEN_HEIGHT
         self.circles = arcade.SpriteList()
+        self.player = None
+        self.sound = arcade.load_sound("Drip Drop.wav")
+        screen_width, screen_height = self.get_viewport_size()
+        SCREEN_WIDTH, SCREEN_HEIGHT = screen_width, screen_height
+        self.game = True
+        self.exit = False
 
     # начальные значения
     def setup(self):
-        for i in range(30):
-            circle = Circle(1)
-            self.circles.append(circle)
+        if self.game:
+            self.music = arcade.play_sound(self.sound, volume= 0.5)
+            self.player = Player()
+            for i in range(50):
+                circle = Circle(random.uniform(0.1, self.player.scale + 0.5))
+                self.circles.append(circle)
 
     # отрисовка объектов
     def on_draw(self):
         arcade.start_render()
         self.circles.draw()
+        self.player.draw()
+        if not self.game:
+            arcade.draw_text("PAUSE", SCREEN_HEIGHT // 2, SCREEN_WIDTH // 2, arcade.color.WHITE, 20)
 
     # логика
     def update(self, delta_time):
-        self.circles.update()
-        while len(self.circles) < 30:
-            circle = Circle(1)
-            self.circles.append(circle)
+        if self.game:
+            self.circles.update()
+            self.player.update()
+            collisions = arcade.check_for_collision_with_list(self.player, self.circles)
+            if len(collisions) > 0:
+                for circle in collisions:
+                    if circle.scale > self.player.scale:
+                        exit()
+                    else:
+                        circle.kill()
+                        self.player.scale += 0.01
+                        if self.player.scale >= 2:
+                            exit()
+            if self.sound.get_stream_position(self.music) == 0:
+                self.music = arcade.play_sound(self.sound, volume= 0.5)
+            while len(self.circles) < 30:
+                circle = Circle(random.uniform(0.1, self.player.scale + 0.5))
+                self.circles.append(circle)
+            if self.exit:
+                exit()
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.SPACE:
+            self.game = False
+        if symbol == arcade.key.LSHIFT:
+            self.game = True
+        if symbol == arcade.key.ESCAPE and self.game:
+            self.exit = True
+
+    def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
+        if self.game:
+            self.player.center_x = x
+            self.player.center_y = y
 
 
 window = OurGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 window.setup()
 arcade.run()
-# цвета от 50 - 200, размеры, класс игрока (коф.сж. - (0.3 - 0.4))
+# TODO сделать паузу
